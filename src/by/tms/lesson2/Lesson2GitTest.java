08a73f2403ecaba160f3bc26167a32284553a585
servenazz <snorkuzieva@gmail.com>
2022-06-08 10:14:11 +0300

    Completed lesson 2 homework

--- src/by/tms/lesson2/Lesson2GitTest.java
+++ src/by/tms/lesson2/Lesson2GitTest.java
@@ -10,11 +10,6 @@ public class Lesson2GitTest {
         double seconds1 = 854;
         System.out.println(seconds1 / 60);
 
-    //    char x = 4;
-    //    char y = Ф;
-    //    char z = (x>>y);
-    //    System.out.println(z);
-
         int firstValue = 12;
         int secondValue = 6;
         System.out.println(firstValue<<secondValue);
