7b53a74db8689aee7f47c3001e22cda309470cf8
servenazz <snorkuzieva@gmail.com>
2022-12-13 00:41:46 +0300

    Add my first class to the project

---
+++ src/by/tms/myproject/lesson1/MyTestClass.java
@@ -0,0 +1,10 @@
+package by.tms.myproject.lesson1;
+
+public class MyTestClass {
+    public static void main(String[] args) {
+        int myFirstVariable = 10;
+        int mySecondVariable = 20;
+   //  myFirstVariable = 1;
+        System.out.println(myFirstVariable+mySecondVariable);
+    }
+}
