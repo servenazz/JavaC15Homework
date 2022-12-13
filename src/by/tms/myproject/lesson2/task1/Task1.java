5b63827d36a51ff9d94fcda6b521c74aabdea205
servenazz <snorkuzieva@gmail.com>
2022-12-13 18:54:41 +0300

    Add my first task to the project

---
+++ src/by/tms/myproject/lesson2/task1/Task1.java
@@ -0,0 +1,10 @@
+package by.tms.myproject.lesson2.task1;
+
+public class Task1 {
+
+    public static void main(String[] args) {
+
+        int number = 8;
+        System.out.println(number%2==0);
+    }
+}
